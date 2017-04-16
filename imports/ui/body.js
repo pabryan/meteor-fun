9db57d67391baf78c6ac719eda57cdd34ac2c4be
Paul Bryan <pabryan@gmail.com>
2017-04-16 23:41:25 +1000

    Added reactive state for showing completed.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,26 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
     tasks() {
-	return Tasks.find({}, { sort : { createdAt: -1}});
+	const instance = Template.instance();
+	if (instance.state.get('hideCompleted')) {
+	    return Tasks.find({ checked: { $ne: true } }, { sort : { createdAt: -1 } });
+	} else {
+	    return Tasks.find({}, { sort : { createdAt: -1 } });
+	}
+    },
+    incompleteCount() {
+	return Tasks.find({ checked: { $ne: true } }).count();
     },
 });
 
@@ -28,5 +41,8 @@ Template.body.events({
 	console.log(event);
 
 	target.text.value = '';
-    }
+    },
+    'change .hide-completed input' (event, instance) {
+	instance.state.set('hideCompleted', event.target.checked);
+    },
 });
