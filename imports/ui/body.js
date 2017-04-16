05832455b8b7bf964514498ca75092a073bdbf06
Paul Bryan <pabryan@gmail.com>
2017-04-16 15:57:04 +1000

    Messing around.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,7 +4,7 @@ import './body.html';
 
 Template.body.helpers({
     tasks: [
-	{ text: "1" },
+	{ text: "1", mess: "one" },
 	{ text: "2", mess: "two" },
 	{ text: "3", mess: "three" },
 	{ text: "4", mess: "four" },
