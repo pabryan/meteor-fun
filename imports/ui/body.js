989f27c1e15e1783c17f788589da024debde66a6
Paul Bryan <pabryan@gmail.com>
2017-04-16 17:12:30 +1000

    Used mongo.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,11 @@
 import { Template } from 'meteor/templating';
 
-import { Tasks } from '../imports/api/tasks.js';
+import { Tasks } from '../api/tasks.js';
 
 import './body.html';
 
 Template.body.helpers({
     tasks() {
 	return Tasks.find({});
-    }
+    },
 });
