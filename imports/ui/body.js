0478b8b8e1cfc9fd00ee780755bbcfa2108a87a9
Paul Bryan <pabryan@gmail.com>
2017-04-16 17:36:32 +1000

    Added input to create tasks.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,6 +6,26 @@ import './body.html';
 
 Template.body.helpers({
     tasks() {
-	return Tasks.find({});
+	return Tasks.find({}, { sort : { createdAt: -1}});
     },
+});
+
+Template.body.events({
+    'submit .new-task'(event) {
+	event.preventDefault();
+
+	const target = event.target;
+	const text = target.text.value;
+
+	Tasks.insert({
+	    text,
+	    createdAt: new Date(),
+	    mess: "Hard coded message",
+	});
+
+	
+	console.log(event);
+
+	target.text.value = '';
+    }
 });
